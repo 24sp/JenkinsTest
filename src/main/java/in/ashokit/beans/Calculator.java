b73adc70cb2b50a0ec0ab6a96f44d1e9cc09826f
Shubham <24shubhamp@gmail.com>
2021-08-06 01:13:15 +0530

    Third Commit

--- src/main/java/in/ashokit/beans/Calculator.java
+++ src/main/java/in/ashokit/beans/Calculator.java
@@ -6,15 +6,15 @@ public class Calculator {
 	{
 		return a + b;
 	}
-<<<<<<< HEAD
+
 	
 	public Integer multiply(Integer a,Integer b)
 	{
 		return a * b;
-=======
+	}
+
 	public void same()
 	{
 		System.out.println("HI SP...");
->>>>>>> 03c43ae6c1194a034dad95c9fd2890a1b26fc68d
 	}
 }
